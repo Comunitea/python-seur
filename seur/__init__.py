1e7c07b962a85d0c967023b1636648300355519d
Raimon Esteve <raimon@nan-tic.com>
2016-11-18 15:15:20 +0100

    Increase version number

--- seur/__init__.py
+++ seur/__init__.py
@@ -1,7 +1,7 @@
 #This file is part of seur. The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 
-__version__ = '0.0.15'
+__version__ = '0.0.16'
 
 __all__ = [
     'API',
